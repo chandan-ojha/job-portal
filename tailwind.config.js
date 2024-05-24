e4ecd879a73b3e068bae3caeab0aff3ea3d18ae9
Chandan Ojha <chandanojha776@gmail.com>
2024-05-24 21:18:50 +0600

    #improve-design

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ export default {
             colors: {
                 "black": "#060606"
             },
+            fontFamily: {
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"]
+            },
+            fontSize: {
+                "2xs": ".625rem" // 10px
+            }
         },
     },
     plugins: [],
