154823c6a418ca28935b159400990120a4d39929
Chandan Ojha <chandanojha776@gmail.com>
2024-05-26 17:27:24 +0600

    #user-authentication-done

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,11 +13,11 @@
         rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black text-white font-hanken-grotesk">
+<body class="bg-black text-white font-hanken-grotesk pb-20">
 <div class="px-10">
     <nav class="flex justify-between items-center py-4 border-b border-white/10">
         <div>
-            <a href="">
+            <a href="/">
                 <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
             </a>
         </div>
@@ -29,9 +29,25 @@
             <a href="#">Companies</a>
         </div>
 
-        <div>
-            <a href="">Post a Job</a>
-        </div>
+        @auth
+            <div class="space-x-6 font-bold flex">
+                <a href="/jobs/create">Post a Job</a>
+
+                <form method="POST" action="/logout">
+                    @csrf
+                    @method('DELETE')
+
+                    <button>Log Out</button>
+                </form>
+            </div>
+        @endauth
+
+        @guest
+            <div class="space-x-6 font-bold">
+                <a href="/register">Sign Up</a>
+                <a href="/login">Log In</a>
+            </div>
+        @endguest
     </nav>
     <main class="mt-10 max-w-[986px] mx-auto">
         {{ $slot }}
