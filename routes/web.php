8ff7ac1b3b42d1b5f7ea24905711eae630091e27
Chandan Ojha <chandanojha776@gmail.com>
2024-05-28 13:00:07 +0600

    #job-post-functionality-done

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
 
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+
 Route::get('/search', SearchController::class);
 Route::get('/tags/{tag:name}', TagController::class);
 
